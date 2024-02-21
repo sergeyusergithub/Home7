335a6fb7e93db2ee078f5cd9fbc2139bf2555cb9
Sergey Sergey <sergeymailnew@mail.ru>
2024-02-21 23:27:40 +0500

    Поправлена ошибка на этапе сборки работы с оператором []. Шаблонный класс определен и реализован в одном заголовочном файле.

--- Home7/dinamic_array.hpp
+++ Home7/dinamic_array.hpp
@@ -1,6 +1,8 @@
 ﻿// dinamic_array.hpp
 
-/*
+/*!
+
+	\brief
 Обязятельными пунктаи задания являются следующие возможности коллекции:
 -- размещение в себе элементов произвольного типа;
 -- добавление и удаление элементов;
@@ -21,14 +23,14 @@
 
 
 template<typename Type>
-// шаблонный класс динамического массива
+/// шаблонный класс динамического массива
 class DinamicArray {
 public:
 
 	// конструктор по умолчанию
 	DinamicArray() :data_{}, size_{} {}
 
-	// конструктор с параметром, параметр - размер динамического массива
+	/// конструктор с параметром, параметр - размер динамического массива
 	DinamicArray(int size) :data_{}, size_(size) {
 		data_ = new Type[size_]{};
 	}
@@ -40,20 +42,38 @@ public:
 	Type& operator[](int index) noexcept;
 
 	const Type& operator[](int index) const noexcept;
-
-
-	Type& Data(int index) {
-		return data_[index];
-	}
-
-	const Type& Data(int index) const{
-		return data_[index];
-	}
-
-
+		
+	// деструктор
 	~DinamicArray() {
 		delete[] data_;
 		size_ = {};
+	}
+
+	//метод возвращающий размер массива
+	int size() const {
+		return size_;
+	}
+
+	// метод добавления элемента к концу  массива, 
+	// при этом массив увеличивает свой размер на добавленный элемент
+	void add(const Type& data);
+
+	/// метод удаления элемента с конца массива,
+	/// при этом массив уменньшает свой массив на удаленный элемент
+	void remove();
+
+	/// возвращает адрес начала массива
+	Type* begin() {
+		return data_;
+	}
+
+
+	/// <summary>
+	/// возвращает адрес конца массива
+	/// </summary>
+	/// <returns></returns>
+	Type* end() {
+		return data_ + size_;
 	}
 
 
@@ -63,8 +83,51 @@ private:
 };
 
 
+//===================================================================
+template<typename Type>
+DinamicArray<Type>::DinamicArray(const DinamicArray<Type>& other)
+{
+	size_ = other.size_;
+	data_ = new Type[size_]{};
+	for (int i = 0; i < size_; ++i) {
+		data_[i] = other.data_[i];
+	}
+}
 
 
 
+//! ===================================================================
+template<typename Type>
+Type& DinamicArray<Type>::operator[](int index) noexcept
+{
+	return data_[index];
+}
+
+
+//==============================================================
+template<typename Type>
+const Type& DinamicArray<Type>::operator[](int index) const noexcept
+{
+	return data_[index];
+}
+
+template<typename Type>
+inline void DinamicArray<Type>::add(const Type& data)
+{
+	Type* tmp = new Type[size_ + 1];
+	for (int i = 0; i < size_; ++i) {
+		tmp[i] = data_[i];
+	}
+	delete[] data_;
+	data_ = tmp;
+	data_[size_] = data;
+	size_ += 1;
+}
+
+template<typename Type>
+inline void DinamicArray<Type>::remove()
+{
+	--size_;
+}
 
 #endif // !DINAMIC_ARRAY_HPP
--- Home7/main.cpp
+++ Home7/main.cpp
@@ -3,68 +3,60 @@
 #include <iostream>
 #include "dinamic_array.hpp"
 
-template <typename T>
-class A {
-public:
-
-	A() {
-		dat = new T[10]{};
+void arr_char(DinamicArray<char>& str, const char* chr) {
+	for (int i = 0; i < str.size(); i++) {
+		str[i] = chr[i];
 	}
-
-
-	T& operator[](int index) {
-		return dat[index];
-	}
-
-	const T& operator[](int index)const {
-		return dat[index];
-	}
-
-	T data() {
-		return dat[0];
-	}
-
-	~A() {
-		delete dat;
-	}
-private:
-	T *dat;
-
-};
-
+}
 
 int main() {
 
 	DinamicArray<int> arr(10);
 
-	A<int> aa;
-
-	aa[1] = 5;
-
-	//arr[3] = 15;
-
-	std::cout << aa[1] << std::endl;
-	std::cout << arr.Data(3) << std::endl;
-	//std::cout << arr[3] << std::endl;
-
-	//int a = arr[0];
-
-	//arr[1] = 5;
-
-	//arr[0] = 1;
+	arr[3] = 15;
 
 	
+	std::cout << arr[3] << std::endl;
+	
+	std::cout << "std::size() = " << std::size(arr) << '\n';
 
-	//arr[0] = 1;
-	//arr[1] = 2;
-	/*for (int i = 0; i < 10; ++i) {
-		std::cout << arr.Data(i) << ", ";
+	arr.add(12);
+
+	std::cout << "std::size() = " << std::size(arr) << '\n';
+
+	std::cout << "arr[10] = " << arr[10] << '\n';
+	std::cout << "arr[9] = " << arr[9] << '\n';
+
+
+	arr.remove();
+
+	std::cout << "std::size() = " << std::size(arr) << '\n';
+
+	for (auto &ar : arr) {
+		std::cout << ar << ' ';
 	}
-	std::cout << "\b\b \n\n";*/
+	std::cout << "\n";
 
-	
+	arr.add(9);
+	for (auto& ar : arr) {
+		std::cout << ar << ' ';
+	}
 
+	DinamicArray<char> str(10);
 
+	std::cout << std::endl;
+
+	str[0] = "HelloWorld"[0];
+
+	std::cout << str[0] << std::endl;
+
+	arr_char(str, "HelloWorld");
+
+	for (auto& st : str) {
+		std::cout << st;
+	}
+
+	std::cout << "\n\n";
 
 	return 0;
 }
